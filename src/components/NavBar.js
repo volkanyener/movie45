a4eff6d0193b55fcd7f4933c17c4264c5c1819cf
volkanyener <volkan__yener@hotmail.com>
2020-09-03 12:23:55 +0200

    navbar done

---
+++ src/components/NavBar.js
@@ -0,0 +1,20 @@
+import React from "react";
+import { NavLink } from "react-router-dom";
+
+export default function NavBar() {
+  return (
+    <div>
+      <NavLink exact={true} activeStyle={{ fontWeight: "bold" }} to="./">
+        Home Page
+      </NavLink>
+      <br />
+      <NavLink activeStyle={{ fontWeight: "bold" }} to="./about">
+        About Page
+      </NavLink>
+      <br />
+      <NavLink activeStyle={{ fontWeight: "bold" }} to="./discover">
+        Discover page
+      </NavLink>
+    </div>
+  );
+}
---
+++ src/pages/DiscoverMoviePage.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function DiscoverMoviePage() {
+  return <div></div>;
+}
